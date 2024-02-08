6c3d317a1451bf5ec48bed60952b6e7f4c772ef2
admin <admin@DESKTOP-1OB8M9D>
2024-02-08 17:52:33 +0900

    2024.02.08

--- oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/dao/EmpDaoImpl.java
+++ oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/dao/EmpDaoImpl.java
@@ -1,21 +1,66 @@
 package com.oracle.oBootMybatis01.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import com.oracle.oBootMybatis01.model.Emp;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
-@RequiredArgsConstructor
+@RequiredArgsConstructor 
 public class EmpDaoImpl implements EmpDao {
+   // Mybatis DB 연동
+   private final SqlSession session;
+   
+   @Override
+   public int totalEmp() {
+      int totEmpCount = 0;
+      System.out.println("EmpDaoImpl Start total...");
+      
+      try {
+         totEmpCount = session.selectOne("com.oracle.oBootMybatis01.EmpMapper.empTotal");
+         System.out.println("EmpDaoImpl totalEmp totEmpCount->"+totEmpCount);
+      } catch(Exception e) {
+         System.out.println("EmpDaoImpl totalEmp Exception->"+e.getMessage());
+      }
+      return totEmpCount;
+   }
 
-	// Mybatis DB 연동
-	private final SqlSession session;
-	
+   @Override
+   public List<Emp> listEmp(Emp emp) {
+	   List<Emp> empList = null;
+	   System.out.println("EmpDaoImpl listEmp Start...");
+	   try {
+		   //                                         Map ID         parameter
+		   empList = session.selectList("tkEmpListAll", emp);
+		   System.out.println("EmpDaoImpl listEmp empList.size() -> " + empList.size());
+	   } catch (Exception e) {
+		   System.out.println("EmpDaoImpl listEmp e.getMessage() -> " + e.getMessage());
+	   }
+	   return empList;
+   }
+
 	@Override
-	public int totalEmp() {
-		
-		return 0;
+	public Emp detailEmp(int empno) {
+		System.out.println("EmpDaoImpl datail start...");
+		Emp emp = new Emp();
+		try {
+			//                                     mapper ID  ,  Parameter   
+			emp = session.selectOne("tkEmpSelOne", empno);
+			System.out.println("EmpDaoImpl detail getName ->" + emp.getEname());
+		} catch (Exception e) {
+			System.out.println("EmpDaoImpl detail Exception -> " + e.getMessage());
+		}
+		return emp;
 	}
-	
+
+	@Override
+	public Emp updateEmp(Emp emp1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/service/EmpServiceImpl.java
+++ oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/service/EmpServiceImpl.java
@@ -1,24 +1,53 @@
 package com.oracle.oBootMybatis01.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.oracle.oBootMybatis01.dao.EmpDao;
+import com.oracle.oBootMybatis01.model.Emp;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class EmpServiceImpl implements EmpService {
+   
+   private final EmpDao ed;
+   
+   @Override
+   public int totalEmp() {
+      System.out.println("EmpServiceImpl start total...");
+      int totEmpCnt = ed.totalEmp();
+      System.out.println("EmpServiceImpl totalEmp totEmpCnt->" + totEmpCnt);
+      
+      return totEmpCnt;
+   }
 
-	private final EmpDao ed;
-	
+   @Override
+   public List<Emp> listEmp(Emp emp) {
+	   List<Emp> empList = null;
+	   System.out.println("EmpServiceImpl listManager start...");
+	   empList = ed.listEmp(emp);
+	   System.out.println("EmpServiceImp listEmp empList.size() -> " + empList.size());
+	   
+	   return empList;
+   }
+
+   @Override
+   public Emp detailEmp(int empno) {
+	   System.out.println("EmpServiceImpl detail...");
+	   Emp emp = null;
+	   emp = ed.detailEmp(empno);
+	   
+	   return emp;
+   }
+
 	@Override
-	public int totalEmp() {
-		System.out.println("EmpServiceImpl Start total...");
-		int totEmpCnt = ed.totalEmp();
-		System.out.println("EmpServiceImpl totalEmp totEmpCnt -> " + totEmpCnt);
-		
-		return totEmpCnt;
+	public Emp updateEmp(Emp emp1) {
+		 System.out.println("EmpServiceImpl update...");
+		 
+		return null;
 	}
 
 }
